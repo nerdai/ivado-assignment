0e177ed280f47e2868f86db444674f0e9b6b42be
nerdai <andrei@nerdai.io>
2023-03-11 14:36:09 -0500

    add modelling CV with bayesian search

--- ivado_assignment/data_cleaner.py
+++ ivado_assignment/data_cleaner.py
@@ -2,6 +2,7 @@
 This module takes the raw data, applies the data cleaning steps identified
 by the EDA. In particular:
 - drop the two records that erronoeuous entries for `feature_6`
+    + cast feature_6 as a numerical feature
 """
 
 from typing import Tuple
@@ -22,6 +23,7 @@ CATS = ['feature_0',
         'feature_8',
         'feature_9'
         ]
+TARGET = 'target'
 
 
 def data_cleaner() -> Tuple[pd.DataFrame, pd.DataFrame]:
@@ -31,17 +33,20 @@ def data_cleaner() -> Tuple[pd.DataFrame, pd.DataFrame]:
         1. complete_df: for a complete-case analysis
         2. incomplete_df: contains incomplete observations to be imputed
     """
-    raw_df = pd.read_csv(RAW_DATA_FILE)
+    incomplete_df = pd.read_csv(RAW_DATA_FILE)
 
     # drop the two records with erroneous entry for feature_6
-    raw_df = raw_df[~raw_df['feature_6'].apply(
+    incomplete_df = incomplete_df[~incomplete_df['feature_6'].apply(
         lambda x: '.' in x)].reset_index(drop=True)
 
-    # complete df
-    complete_df = raw_df[~raw_df.isna().any(axis=1)].reset_index(drop=True)
+    # cast to correct data types
+    for feat in CATS + [TARGET]:
+        incomplete_df[feat] = pd.Categorical(incomplete_df[feat])
+    incomplete_df[TARGET] = incomplete_df[TARGET].cat.codes  # Y = 1; N = 0
 
-    # imputed df
-    incomplete_df = raw_df
+    # complete df
+    complete_df = incomplete_df[~incomplete_df.isna().any(
+        axis=1)].reset_index(drop=True)
 
     return complete_df, incomplete_df
 
@@ -50,11 +55,11 @@ def main():
     """
     Main method containing logic for this script.
     """
-    complete, imputed = data_cleaner()
+    complete, incomplete_df = data_cleaner()
 
     # store dfs to local
     complete.to_csv("./data/processed/complete_df.csv")
-    imputed.to_csv("./data/processed/incomplete_df.csv")
+    incomplete_df.to_csv("./data/processed/incomplete_df.csv")
 
 
 if __name__ == "__main__":
