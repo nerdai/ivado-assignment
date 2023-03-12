f84bba2c17996c6f73b643d0c53ef0bbf00212c4
nerdai <andrei@nerdai.io>
2023-03-12 01:56:43 -0500

    lint and format data_loader.py:

--- ivado_assignment/utils/data_loader.py
+++ ivado_assignment/utils/data_loader.py
@@ -1,17 +1,16 @@
 """
-Utils module containing utility functions used by other modules.
+Module for loading data and performing any necessary preparation steps.
 """
 
 import pandas as pd
 from ivado_assignment.settings.data import config
 
+
 def load_and_prep(csv_path: str) -> pd.DataFrame:
     """
-    Utility function for loading a csv into a pd.DataFrame
-    params:
-    [] train_csv_path: str
-    returns:
-    [] pd.DataFrame
+    Utility function for loading a csv into a pd.DataFrame and casting the
+    categorical fields as string, which is necessary prior to passing it to
+    downstream sklearn pipeline.
     """
     df = pd.read_csv(csv_path)
     for feat in config['categorical']:
