57cbabaa2969d65963a4aea74d4bc567c174e267
nerdai <andrei@nerdai.io>
2023-03-12 01:18:24 -0500

    lint and format get_metrics.py

--- ivado_assignment/bin/get_metrics.py
+++ ivado_assignment/bin/get_metrics.py
@@ -1,12 +1,20 @@
 """
-This file produces the classification metrics report from a supplied labels
-as well as predictions data files.
+This file produces the classification report from a supplied labels
+as well as predictions data files. It also outputs in the console other 
+suchs log-loss, Brier's score, F1 Score, and ROC-AUC Score.
 """
 
 import argparse
 import pandas as pd
+from sklearn.metrics import (
+    brier_score_loss,
+    classification_report,
+    confusion_matrix,
+    roc_auc_score,
+    f1_score,
+    log_loss
+)
 from ivado_assignment.settings.data import config
-from sklearn import metrics
 
 parser = argparse.ArgumentParser(
     prog='IVADO take home assignment',
@@ -22,21 +30,28 @@ parser.add_argument('--labels', type=str, required=True,
 def produce_report():
     """
     This function loads user specified labels and preds files and produces
-    the sklearn metrics.
+    the sklearn 
     """
     # load data
     args = parser.parse_args()
-    labels = pd.read_csv(args.labels)[[config.id_col, config.target]]
+    labels = pd.read_csv(args.labels)[[config['id_col'], config['target']]]
     preds = pd.read_csv(args.preds)
 
     # metrics
-    results = pd.merge(preds, labels, on=config.id_col)
-    print(metrics.classification_report(results[config.target], results.pred))
-    print("Confusion:\n", metrics.confusion_matrix(results[config.target], results.pred), "\n")
-    print("ROC-AUC: ", metrics.roc_auc_score(results[config.target], results.pred_proba))
-    print("Log loss: ", metrics.log_loss(results[config.target], results.pred_proba))
-    print("Brier: ", metrics.brier_score_loss(results[config.target], results.pred_proba))
-    print("F1 Score:", metrics.f1_score(results[config.target], results.pred))
+    results = pd.merge(preds, labels, on=config['id_col'])
+    print(classification_report(
+        results[config['target']], results.pred))
+    print("Confusion:\n", confusion_matrix(
+        results[config['target']], results.pred), "\n")
+    print("ROC-AUC: ",
+          roc_auc_score(results[config['target']], results.pred_proba))
+    print("Log loss: ", log_loss(
+        results[config['target']], results.pred_proba))
+    print("Brier: ", brier_score_loss(
+        results[config['target']], results.pred_proba))
+    print("F1 Score:", f1_score(
+        results[config['target']], results.pred))
+
 
 if __name__ == "__main__":
     produce_report()
